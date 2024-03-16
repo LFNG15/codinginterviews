96cffdbba38b4e6a6a2eba26adb9f3377105c514
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-03-16 17:44:00 -0300

    Feat: Remove Element

---
+++ Array-String/Remove Element/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int RemoveElement(vector<int> &nums, int val)
+    {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                ++k;
+            }
+        }
+        return k;
+    }
+};
